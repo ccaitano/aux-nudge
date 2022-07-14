a5e1da0189ae4bb59132fbc65d2cf38b9903f37d
ccaitano <cheryl.caitano@gmail.com>
2022-07-13 18:06:58 -1000

    Random Word Generator works on button click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Genius Lyrics API
 const options = {
 	method: 'GET',
 	headers: {
@@ -6,12 +7,43 @@ const options = {
 	}
 };
 
-function testAPI(keyword) {
-var lyricsAPI = `https://genius-song-lyrics1.p.rapidapi.com/search/multi?q="${keyword}"&per_page=3&page=1`
-fetch(lyricsAPI, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+//Words API
+const options2 = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '04d9c7ad81mshe5a167cf677bb86p1dd6e4jsnd372da947ed0',
+		'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+	}
+};
+
+//Get Random Word
+function getRandomWord() {
+	fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', options2)
+		.then(response => response.json())
+		.then(function (data){
+			var randomWord = data.word;
+			console.log(randomWord);
+			var searchBarEl = document.getElementById("search-bar");
+			searchBarEl.setAttribute("value", randomWord);
+		})
+		.catch(err => console.error(err));
+	
 }
-var keyword = "accupuncture"
-testAPI(keyword);
+var randomButtonEl = document.getElementById("random-button");
+randomButtonEl.addEventListener("click", getRandomWord);
+
+
+//Search for Songs and Display Results
+function searchSongs(keyword) {
+	var lyricsAPI = `https://genius-song-lyrics1.p.rapidapi.com/search/multi?q="${keyword}"&per_page=5&page=1`
+	fetch(lyricsAPI, options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
+}
+
+//Open Modal
+
+	//YouTube URL search link
+
+//Save Tracks
