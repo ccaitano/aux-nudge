bf0c3c640fc68896cfab648f3254ddcd8229b501
alexmayberry <alexclimbs@gmail.com>
2022-07-13 21:47:11 -0700

    created the searchWord function to use the search bar input to search the song database and return results. Currently does not work.

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@ const options2 = {
 	}
 };
 
+// Store search bar input in searchBarEl
+var searchBarEl = document.getElementById("search-bar");
+
 //Get Random Word
 function getRandomWord() {
 	fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', options2)
@@ -23,7 +26,6 @@ function getRandomWord() {
 		.then(function (data){
 			var randomWord = data.word;
 			console.log(randomWord);
-			var searchBarEl = document.getElementById("search-bar");
 			searchBarEl.setAttribute("value", randomWord);
 		})
 		.catch(err => console.error(err));
@@ -32,15 +34,24 @@ function getRandomWord() {
 var randomButtonEl = document.getElementById("random-button");
 randomButtonEl.addEventListener("click", getRandomWord);
 
+var searchTerm = searchBarEl.value
+//Must convert spaces in searchTerm to "%20" to create a valid url
 
 //Search for Songs and Display Results
-function searchSongs(keyword) {
-	var lyricsAPI = `https://genius-song-lyrics1.p.rapidapi.com/search/multi?q="${keyword}"&per_page=5&page=1`
-	fetch(lyricsAPI, options)
+function searchWord() {
+	// Add search term from #search-bar into url for search
+	fetch(`https://genius-song-lyrics1.p.rapidapi.com/search?q=${searchTerm}&per_page=10&page=1`, options)
 		.then(response => response.json())
-		.then(response => console.log(response))
+		.then(function (data){
+			searchDomain = data.response.hits[0].result.title;
+			console.log(searchDomain);
+			console.log('test');
+		})
 		.catch(err => console.error(err));
+	
 }
+
+searchWord();
 
 //Open Modal
 
